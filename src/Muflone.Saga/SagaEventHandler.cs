5e2a60ab86e0a14bc12166fe19c74f526786a49e
Alberto <alberto.acerbis@4solid.it>
2020-07-24 18:30:05 +0200

    Saga
    - Add Handler for StartedBy and SagaEvent in Muflone.Saga

---
+++ src/Muflone.Saga/SagaEventHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Muflone.Messages.Events;
+using Muflone.Saga.Persistence;
+
+namespace Muflone.Saga
+{
+    public abstract class SagaEventHandler<TEvent> : ISagaEventHandler<TEvent> where TEvent : Event
+    {
+        protected readonly IServiceBus ServiceBus;
+        protected readonly ISagaRepository Repository;
+
+        protected SagaEventHandler(IServiceBus serviceBus, ISagaRepository repository)
+        {
+            this.ServiceBus = serviceBus;
+            this.Repository = repository;
+        }
+
+        public abstract Task Handle(TEvent @event);
+
+        #region Dispose
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~SagaEventHandler()
+        {
+            Dispose(false);
+        }
+
+
+        #endregion
+    }
+}
